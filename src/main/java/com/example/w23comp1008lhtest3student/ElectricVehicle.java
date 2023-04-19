10df4278bb5c89e6cdacaa1f9069061fd11ec21d
IDWilks <122938132+IDWilks@users.noreply.github.com>
2023-04-19 09:14:54 -0400

    Question 6 complete

--- src/main/java/com/example/w23comp1008lhtest3student/ElectricVehicle.java
+++ src/main/java/com/example/w23comp1008lhtest3student/ElectricVehicle.java
@@ -1,21 +1,28 @@
 package com.example.w23comp1008lhtest3student;
 
-public class ElectricVehicle {
+public class ElectricVehicle extends Car{
     private int range;
 
     public ElectricVehicle(String make, String model, double price, int year, int range) {
-
+        super(make, model, price, year);
+        setRange(range);
     }
 
     public int getRange() {
         return range;
     }
 
-    public void setRange(int range) {
+    public void setRange (int range) throws IllegalArgumentException{
+        if(range >= 200 && range <= 600 ){
+            this.range = range;
+        }
+        else{
+            throw new IllegalArgumentException("Please enter a valid range between 200-600km");
+        }
     }
 
-    public String toString()
-    {
-        return null;
+    public String toString(Car car) {
+        String ad = super.toString(car) + "-Electric";
+        return ad;
     }
 }
